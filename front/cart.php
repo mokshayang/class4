a981cba2025622f43be627afc391d427c1f1be7d
unknown <dullstart.yang.com>
2023-06-04 23:25:46 +0800

    修改會員登入時，直接點選購物車的問題

--- front/cart.php
+++ front/cart.php
@@ -1,20 +1,22 @@
 <?php
-
-if (isset($_GET['id'])) {
+if(isset($_GET['id'])){
     $_SESSION['cart'][$_GET['id']] = $_GET['qt'];
 }
-if (!isset($_SESSION['mem'])) {
+if(!isset($_SESSION['mem'])){
     to("index.php");
 }
-
 ?>
 <style>
     .num input{
-        border: 0;
+        order:0;
         background-color: transparent;
+        width: 40px;
+    }
+    .bbt{
+        width: 100px;
     }
 </style>
-<h2 class="ct"><?= $_SESSION['mem'] ?>的購物車</h2>
+<h2 class="ct"><?=$_SESSION['mem']?>的購物車</h2>
 <table class="all">
     <tr class="ct tt">
         <td>編號</td>
@@ -25,64 +27,61 @@ if (!isset($_SESSION['mem'])) {
         <td>小記</td>
         <td>刪除</td>
     </tr>
-
-    <?php
-    foreach ($_SESSION['cart'] as $id => $qt) {
+    <?php  
+    if(isset($_SESSION['cart'])){
+    foreach($_SESSION['cart'] as $id => $qt){
         $row = $Goods->find($id);
     ?>
-        <tr class="ct pp content">
-            <td><?= $row['no'] ?></td>
-            <td><?= $row['name'] ?></td>
-            <td class="num">
-                <input type="text" id="<?= $id ?>" value="<?= $qt ?>" onchange="num(<?= $id ?>)" style="width:40px;">
-            </td>
-            <td><?= $row['stock'] ?></td>
-            <td><?= $row['price'] ?></td>
-            <td><?= $row['price'] * $qt ?></td>
-            <td >
-                <img src="icon/0415.jpg" class="cu" onclick="remove(this,<?= $row['id'] ?>)">
-            </td>
-        </tr>
-    <?php } ?>
+    <tr class="ct pp content">
+        <td><?=$row['no']?></td>
+        <td><?=$row['name']?></td>
+        <td class="num">
+            <input type="text" id="<?=$id?>" value="<?=$qt?>" onchange="getNum(<?=$row['id']?>)">
+        </td>
+        <td><?=$row['stock']?></td>
+        <td><?=$row['price']?></td>
+        <td><?=$row['price']*$qt?></td>
+        <td>
+            <img src="icon/0415.jpg" class="cu" onclick="remove(this,<?=$row['id']?>)">
+        </td>
+    </tr>
+    <?php } } ?>
 </table>
-
-<div class="ct" id="sub">
+<div class="ct ord">
     <?php
-    if (empty($qt)) {
+    if(empty($qt) || !isset($_SESSION['cart'])){
     ?>
-        <br> 您的購物車是空的 <br><br>
-        <a href="?do=index.php"><img src="icon/0411.jpg" class="cu" style="width:100px"></a>
-    <?php } else { ?>
-        <a href="?do=index.php"><img src="icon/0411.jpg" class="cu" style="width:100px"></a>
-        <a href="?do=ord"><img src="icon/0412.jpg" class="cu" style="width:100px"></a>
+        <br>您的購物車是空的<p></p>
+        <a href="index.php"><img src="icon/0411.jpg" class="bbt"></a>
+    <?php }else{ ?>
+        <a href="index.php"><img src="icon/0411.jpg" class="bbt"></a>
+        <a href="?do=ord"><img src="icon/0412.jpg" class="bbt"></a>
     <?php } ?>
 </div>
-
-
-
 <script>
-    function num(id) {
-        let num = $('#' + id).val();
-        $.post("api/num.php", {id,num})
+    function getNum(id){
+        let num = $('#'+id).val()
+        $.post("api/num.php",{id,num})
+        let url = new URL(window.location.href);
+        url.searchParams.set('qt', num);
+        history.pushState(null, null, url.toString());
     }
-
-    function remove(dom, id) {
+    function remove(dom,id){
         let div = `
-        <br> 您的購物車是空的 <br><br>
-                <a href="?do=index.php">
-                    <img src="icon/0411.jpg" class="cu" style="width:100px">
-                </a>
-                `
-        $.post("api/remove.php", {
-            id
-        }, () => {
-            $(dom).parents('tr').remove();
-            history.pushState(null, null, "?do=cart");
-            let table = $('.all');
-            let row = table.find('.content');
-            if (row.length == 0) {
-                $('#sub').empty().append(div)
+                    <br>您的購物車是空的<p></p>
+                    <a href="index.php">
+                        <img src="icon/0411.jpg" class="bbt">
+                    </a>
+                    `
+        $.post("api/remove.php",{id},()=>{
+            $(dom).parents('tr').remove()
+            history.pushState(null,null,'?do=cart')
+            let table = $('.all')
+            let row = table.find('countent')
+            if(row.length == 0){
+                $('.ord').remove().append(div)
             }
         })
     }
 </script>
+
